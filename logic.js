1c998b0a7413f49bc6953227251474dad8be6f39
Abdur Rehman <135581995+Rehman-codes@users.noreply.github.com>
2024-06-12 15:22:37 +0500

    Toggle view Functionality Completed

--- logic.js
+++ logic.js
@@ -1,4 +1,27 @@
 
+function toggleView(e)
+{
+    let addTaskButton = document.getElementById("add-task");
+    let viewTasksButton = document.getElementById("view-tasks");
+    let allTasksDiv = document.getElementById("all-tasks");
+    let form = document.getElementById("add-task-form");
+    let buttonID = e.target.id;
+
+    if(buttonID === "add-task")
+    {
+       addTaskButton.style.backgroundColor = "white";
+       viewTasksButton.style.backgroundColor = "#D5FF98";
+       allTasksDiv.style.display = "none";
+       form.style.display = "flex";
+    }  
+    else
+    {
+       addTaskButton.style.backgroundColor = "#D5FF98";
+       viewTasksButton.style.backgroundColor = "white";
+       allTasksDiv.style.display = "block";
+       form.style.display = "none";
+    }
+}
 
 function addTask()
 {
